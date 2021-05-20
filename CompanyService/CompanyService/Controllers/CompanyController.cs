85016da03f23e78647c178a03846d2b5a2705157
Bora KAŞMER <bora@borakasmer.com>
2021-05-21 02:59:56 +0300

    Create Project

---
+++ CompanyService/CompanyService/Controllers/CompanyController.cs
@@ -0,0 +1,65 @@
+﻿using CompanyService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        public CompanyController()
+        {
+
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Add New CompanyName To The RabbitMQ.", Description = "<h2>Trigger Go Microservice</h2> <br> When You Add new Company to the Queue, It will insert to the SQLDB and Get Latest 10 Articles from borakasmer.com")]
+        public bool InsertCompany(Company companyModel)
+        {
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "127.0.0.1",
+                    Password = "guest",
+                    Port = 5672,
+                    VirtualHost = "/",
+                };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "company",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var companyData = JsonConvert.SerializeObject(companyModel);
+                    var body = Encoding.UTF8.GetBytes(companyData);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "company",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine($"{companyModel.CompanyName} is Send to the queue");
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ CompanyService/CompanyService/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyService.Models
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public float CompanyCode { get; set; }
+        public string CompanyUrl { get; set; }
+        public string ApiUrl { get; set; }
+    }
+}
